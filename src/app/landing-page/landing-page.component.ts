8d6d9352a421138c8bf7b4cc7f8b6164f6c2579b
Stefanizzi Valentino <valentino.stefanizzi@euris.it>
2025-03-25 08:16:35 +0100

    refactor: simplify landing page animations and update styles

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
 import { TranslatePipe } from '@ngx-translate/core';
 import { TranslationService } from '../services/translation.service';
 import { CommonModule } from '@angular/common';
@@ -8,21 +7,26 @@ import { CommonModule } from '@angular/common';
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
-  animations: [
-    trigger('fadeIn', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('1s 2s', style({ opacity: 1 }))
-      ])
-    ])
-  ],
   imports: [
     TranslatePipe,
     CommonModule
   ]
 })
 
-export class LandingPageComponent{
+export class LandingPageComponent {
   animationState = false;
+  showContent = false;
+  showOverlay = true;
+
   translationService = inject(TranslationService);
+
+  startAnimation() {
+    this.animationState = true;
+    setTimeout(() => {
+      this.showOverlay = false;
+    }, 2500);
+    setTimeout(() => {
+      this.showContent = true;
+    }, 300);
+  }
 }
